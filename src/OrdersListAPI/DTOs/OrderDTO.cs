ce87553672b0b518109fad0c73671bd500aa1750
TonyTokarchuk <arogggh@gmail.com>
2020-01-16 12:16:28 +0200

    Change validation.

--- src/OrdersListAPI/DTOs/OrderDTO.cs
+++ src/OrdersListAPI/DTOs/OrderDTO.cs
@@ -4,7 +4,6 @@ namespace OrdersListAPI.DTOs
 {
     public class OrderDTO
     {
-        [RegularExpression(@"^[0-9-]*$", ErrorMessage = "Only digits allowed")]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "ProductId is required")]
@@ -15,8 +14,6 @@ namespace OrdersListAPI.DTOs
         [RegularExpression(@"^[0-9-]*$", ErrorMessage = "Only digits allowed")]
         public int StatusId { get; set; }
 
-        [Required(ErrorMessage = "Count is required")]
-        [RegularExpression(@"^[0-9-]*$", ErrorMessage = "Only digits allowed")]
         public int Count { get; set; }
     }
 }
